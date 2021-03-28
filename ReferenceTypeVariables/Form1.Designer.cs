5c1a67e974472461e65ef74a85f3b832ec21107c
Emre Uzun <emre.uzun@sibertechno.com>
2021-03-28 22:31:07 +0300

    Add project files.

---
+++ ReferenceTypeVariables/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace ReferenceTypeVariables
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvObject = new System.Windows.Forms.DataGridView();
+            this.dgvDb = new System.Windows.Forms.DataGridView();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvObject)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvObject
+            // 
+            this.dgvObject.AllowUserToAddRows = false;
+            this.dgvObject.AllowUserToDeleteRows = false;
+            this.dgvObject.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvObject.Location = new System.Drawing.Point(12, 83);
+            this.dgvObject.Name = "dgvObject";
+            this.dgvObject.ReadOnly = true;
+            this.dgvObject.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvObject.Size = new System.Drawing.Size(373, 355);
+            this.dgvObject.TabIndex = 0;
+            this.dgvObject.SelectionChanged += new System.EventHandler(this.dgvDb_SelectionChanged);
+            // 
+            // dgvDb
+            // 
+            this.dgvDb.AllowUserToAddRows = false;
+            this.dgvDb.AllowUserToDeleteRows = false;
+            this.dgvDb.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDb.Location = new System.Drawing.Point(415, 83);
+            this.dgvDb.Name = "dgvDb";
+            this.dgvDb.ReadOnly = true;
+            this.dgvDb.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvDb.Size = new System.Drawing.Size(373, 355);
+            this.dgvDb.TabIndex = 1;
+            this.dgvDb.SelectionChanged += new System.EventHandler(this.dgvDb_SelectionChanged);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(72, 24);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ad :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(198, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Yaş :";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(239, 24);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 20);
+            this.txtAge.TabIndex = 4;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(369, 22);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Güncelle";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.dgvDb);
+            this.Controls.Add(this.dgvObject);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvObject)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDb)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvObject;
+        private System.Windows.Forms.DataGridView dgvDb;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ ReferenceTypeVariables/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReferenceTypeVariables
+{
+    public partial class Form1 : Form
+    {
+        private dbVariableTypeTestEntities _db;
+        private List<User> users;
+        User selectedUser;
+        string selectedGridName;
+        public Form1()
+        {
+            InitializeComponent();
+            _db = new dbVariableTypeTestEntities();
+
+            if (_db.Users.Count()==0)
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    _db.Users.Add(new User()
+                    {
+                        ID = Guid.NewGuid(),
+                        Age = i,
+                        Name = "name" + i.ToString()
+                    });
+                }
+                _db.SaveChanges();
+            }
+            users = _db.Users.ToList();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshDataGrids();
+        }
+
+        public void RefreshDataGrids()
+        {
+            dgvDb.DataSource = null;
+            dgvObject.DataSource = null;
+            dgvDb.DataSource = _db.Users.ToList();
+            dgvObject.DataSource = users.ToList();
+        }
+
+        private void dgvDb_SelectionChanged(object sender, EventArgs e)
+        {
+            selectedUser = new User();
+            DataGridView grid = (DataGridView)sender;
+            if (grid.SelectedRows.Count > 0)
+            {
+                int selectedrowindex = grid.SelectedRows[0].Index;
+                DataGridViewRow selectedRow = grid.Rows[selectedrowindex];
+                selectedUser.ID = (Guid)selectedRow.Cells["ID"].Value;
+                txtName.Text = selectedUser.Name = selectedRow.Cells["Name"].Value.ToString();
+                selectedUser.Age = Convert.ToInt32(selectedRow.Cells["Age"].Value);
+                txtAge.Text = selectedUser.Age.ToString();
+                selectedGridName = grid.Name;
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (selectedGridName=="dgvDb")
+            {
+                var user = _db.Users.Where(i => i.ID == selectedUser.ID).FirstOrDefault();
+                user.Name = txtName.Text;
+                user.Age = int.Parse(txtAge.Text);
+                _db.SaveChanges();
+            }
+            else
+            {
+                var user = users.Where(i => i.ID == selectedUser.ID).FirstOrDefault();
+                user.Name = txtName.Text;
+                user.Age = int.Parse(txtAge.Text);
+            }
+            RefreshDataGrids();
+        }
+    }
+}
